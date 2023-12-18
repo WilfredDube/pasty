a9b91d92ee77bb4de9715a5d19c85ff89a7ca3da
Wilfred Dube <wilfreddube@gmail.com>
2023-12-18 08:54:17 +0200

    serve html and static assets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	router.LoadHTMLGlob("./ui/html/pages/*")
+	router.Static("/assets", "./ui/static/")
+
 	router.GET("/", home)
 	router.GET("/paste/view", snippetView)
 	router.POST("/paste/create", snippetCreate)
