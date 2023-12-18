9bdf02a0c8954b4f1f8b132e7973b1a8c4e10b6d
Wilfred Dube <wilfreddube@gmail.com>
2023-12-18 08:52:56 +0200

    add home page template and updated handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,9 +8,7 @@ import (
 )
 
 func home(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello from Pasty",
-	})
+	c.HTML(http.StatusOK, "home.tmpl", nil)
 }
 
 func snippetView(c *gin.Context) {
